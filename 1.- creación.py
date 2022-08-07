181edc30f1b078b5508527daa45fd572f0e35446
Axell Concha Regatto <41928326+FKSAxell@users.noreply.github.com>
2022-08-07 01:42:26 -0500

    Update 1.- creación.py

--- 1.- creación.py
+++ 1.- creación.py
@@ -3,10 +3,18 @@ import numpy as np
 # Numpy utiliza una estructura de datos para manejo de vectores y matrices
 # denominada array o arreglo.
 
-arr = np.array([])
+arr = np.array([1, 2, 3, 4, 5])
 
 # Es muy similar a las listas en python, con la diferencia que los elementos
 # de un arreglo deben ser del mismo tipo
+
+#Index en Vectores
+elemento = arr[0]
+print('elemento: ', elemento)
+
+#Slicing en Vectores
+parte = arr[0:3]
+print('parte: ', parte)
 
 a = np.array(range(10), float)
 print('a: ', a)
@@ -30,4 +38,8 @@ print('f: ', f)
 
 #Función zeros
 g = np.zeros(7)
+print('g: ', g)
+
+g.fill(2)
+
 print('g: ', g)
